ad6c349b5d6340f9dced0de793c1a1ab2e5c142f
Luciano Freitas <lucianoguns123@gmail.com>
2023-04-26 00:38:34 -0300

    add cat photos

--- ludoug-portfolio/src/App.js
+++ ludoug-portfolio/src/App.js
@@ -1,15 +1,39 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.jpg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    fetchCat();
+  }, []);
+
+  function fetchCat() {
+    fetch('https://api.thecatapi.com/v1/images/search')
+      .then((res) => res.json())
+      .then((result) => setData(result[0].url)
+      );
+  }
+
+  if (data === '') return 'Loading...';
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Meu portifólio está em construção, mas em breve estará disponível
-        </p>
+          <img src={logo} alt="logo" width="200" height="200" className='perfil'/>
+          Meu portifólio está em construção, mas em breve estará disponível<br/>
+          Até lá se divirta gerando fotos aleatórias de gatos com o botão abaixo
       </header>
+      <br/>
+      <body>
+      <div>
+      <div className='All'>
+        <button className='myButton' type="button" onClick={fetchCat}>Meow 🐈</button>
+      </div>
+      <br/>
+        <img src={data} alt="imagem gatinho" className='catImg'/>
+    </div>
+      </body>
     </div>
   );
 }
