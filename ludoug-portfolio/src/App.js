1cb1dec8c5e6b94bb6136cfbd9fccd7c5694cfdf
Luciano Freitas <lucianoguns123@gmail.com>
2023-04-25 23:56:18 -0300

    first commit

---
+++ ludoug-portfolio/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Meu portifólio está em construção, mas em breve estará disponível
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
